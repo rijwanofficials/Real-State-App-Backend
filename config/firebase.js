0a70050b45fe44797cf605312115f41408d362f3
Rijwan Husain <husainrijwan2001@gmail.com>
2025-10-08 23:50:00 +0530

    update Firebase configuration to use environment variables instead of a service account file

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,21 @@
 const admin = require("firebase-admin");
-const path = require("path");
 
-const serviceAccount = require(path.resolve(__dirname, "./serviceAccountKey.json"));
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // convert \n to real newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    projectId: serviceAccount.project_id, // make sure projectId is passed
+    projectId: serviceAccount.project_id,
   });
   console.log("<<------✅ Firebase Admin initialized with project------->>");
 }
