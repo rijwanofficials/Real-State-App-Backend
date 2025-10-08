9f2ccda650d2eb9ca0f9fdd6336247fca971132e
Rijwan Husain <husainrijwan2001@gmail.com>
2025-10-08 22:28:48 +0530

    update app.js with CORS configuration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('module-alias/register');
 require("dotenv").config();
-const connectDB = require("./config/db"); // MongoDB connection
+const connectDB = require("./config/db");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -13,55 +13,45 @@ const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
 
 const app = express();
 
-// ------------------------
+
+
+//CORS Configuration
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://real-state-app-xi.vercel.app/",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+
 // Connect to MongoDB
-// ------------------------
 connectDB().then(() => {
     console.log("<<---✅ MongoDB connection stablished before starting server--->>");
 
-    // ------------------------
     // Start Server after DB connected
-    // ------------------------
     app.listen(PORT, () => {
         console.log(`<<----- ✅ Server started on PORT ${PORT} ------>>`);
     });
 });
 
-// ------------------------
-// Middleware
-// ------------------------
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true,
-    })
-);
-
 app.use(morgan("dev"));           // Logging
 app.use(express.json());          // Parse JSON bodies
 app.use(cookieParser());          // Parse cookies
 
-// ------------------------
 // Routes
 // ------------------------
 app.use('/api/v1', apiRouter);
 
-// ------------------------
 // Health check endpoint
-// ------------------------
 app.get("/", (req, res) => {
     res.send("✅ Backend is Running 🚀");
 });
 
-// ------------------------
 // 404 Handler
-// ------------------------
 app.use((req, res) => {
     res.status(404).json({
         success: false,
@@ -69,9 +59,7 @@ app.use((req, res) => {
     });
 });
 
-// ------------------------
 // Global Error Handler
-// ------------------------
 app.use((err, req, res, next) => {
     console.error("❌ Server Error:", err);
     res.status(500).json({
